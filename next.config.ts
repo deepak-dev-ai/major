449e17ba502edc406a1c8323acad75d26709c27c
Deepak Kumar <deepakkumar82906@gmail.com>
2025-08-18 23:26:08 +0530

    Ready to deply part 3

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb", // optional
+      allowedOrigins: ["*"], // optional
+    },
+  },
   images: {
     remotePatterns: [
       {
+        protocol: "https",
         hostname: "**",
       },
     ],
